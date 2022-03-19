5a6ce8b608678d47361f04fe0d5163f7b4b1223c
Syed mozammil karim <47852028+mozammilkarim@users.noreply.github.com>
2022-03-19 18:43:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const products = [
-    { name: "Agha Noor", img: "../productDetails/aghaNoor.jpg", description: "Pure cotton Lawn suit Long 2.04 top Cotton dupatta 2.02mtr", price: 750, variations: [] },
-    { name: "Banarsi print", img: "../productDetails/banarsi.jpg", description: "Banarasi print suit 2 mt top 2.50 mt bottom,2 mt dupatta", price: 500, variations: ['Red', 'Pink', 'Sea green'] },
-    { name: "Chicken lakhnavi", img: "../productDetails/chickenLakhnavi.jpg", description: "Pure cotton chicken work Full lakhnavi work suit Heavy cutwork Dupatta", price: 1000, variations: ['Peach', 'Pink', 'Yellow', 'Green'] },
-    { name: "cotton Namazi Dupatta", img: "../productDetails/cotton Namazi Dupatta.jpg", description: "Printed cotton suitPrinted bottom Printed pure cotton dupatta", price: 550, variations: ['Peach', 'Pink', 'Yellow', 'Blue'] },
-    { name: "Fox georgette multi sequence ", img: "../productDetails/Fox georgette multi sequence.jpg", description: "Multi sequence work suit Heavy border daman Stylish bottom suit Heavy dupatta", price: 1200, variations: ['Grey', 'Pink', 'Yellow', 'grape green'] },
-    { name: "Georgette suits(boutique collections)", img: "../productDetails/Georgette suits(boutique collections).jpg", description: "Geogette sequence suit Designer embroidery suit", price: 850, variations: ['Peach', 'Yellow', 'Pink', 'Sea green'] },
-    { name: "Patch lawn suit", img: "../productDetails/patchLawn.jpg", description: "Pure lawn cotton suit Fancy embroidery with patch  Cotton dupatta", price: 1000, variations: ['Blue', 'Yellow', 'Sea green', 'Black'] },
-    { name: "Party wear gowns", img: "../productDetails/Party wear gowns.jpg", description: "Net fabric with heavy embroidery Santoon lining", price: 800, variations: ['Royal Blue', 'Pista green'] },
-    { name: "Gun gun collection", img: "../productDetails/Gun gun collection.jpg", description: "", price: 500, variations: ['Tomato Red', 'Pink', 'Turmeric Yellow', 'Green'] },
-    { name: "Jamm cotton suit", img: "../productDetails/jamm cotton suit.jpg", description: "Pure jamm cotton with hand embroidery 2mt top 2.5mt botton Embroidered dupatta", price: 525, variations: ['Red', 'Orange', 'Yellow', 'Green'] },
-    { name: "Lehanga", img: "../productDetails/Lehanga.jpg", description: "net fabric lehanga with heavy embroidery design bell lace heavy dupatta,adjustable blouse with latkan and doori", price: 1000, variations: ['Green'] },
-    { name: "Mumtaz Suit", img: "../productDetails/Mumtaz Suit.png", description: "Very Famous suit", price: 650, variations: [] },
-    { name: "South cotton", img: "../productDetails/south cotton.jpg", description: "Print cotton bottom Print Dupatta", price: 750, variations: ['Purple','Orange'] }
+    { name: "Agha Noor", img: "./productDetails/aghaNoor.jpg", description: "Pure cotton Lawn suit Long 2.04 top Cotton dupatta 2.02mtr", price: 750, variations: [] },
+    { name: "Banarsi print", img: "./productDetails/banarsi.jpg", description: "Banarasi print suit 2 mt top 2.50 mt bottom,2 mt dupatta", price: 500, variations: ['Red', 'Pink', 'Sea green'] },
+    { name: "Chicken lakhnavi", img: "./productDetails/chickenLakhnavi.jpg", description: "Pure cotton chicken work Full lakhnavi work suit Heavy cutwork Dupatta", price: 1000, variations: ['Peach', 'Pink', 'Yellow', 'Green'] },
+    { name: "cotton Namazi Dupatta", img: "./productDetails/cotton Namazi Dupatta.jpg", description: "Printed cotton suitPrinted bottom Printed pure cotton dupatta", price: 550, variations: ['Peach', 'Pink', 'Yellow', 'Blue'] },
+    { name: "Fox georgette multi sequence ", img: "./productDetails/Fox georgette multi sequence.jpg", description: "Multi sequence work suit Heavy border daman Stylish bottom suit Heavy dupatta", price: 1200, variations: ['Grey', 'Pink', 'Yellow', 'grape green'] },
+    { name: "Georgette suits(boutique collections)", img: "./productDetails/Georgette suits(boutique collections).jpg", description: "Geogette sequence suit Designer embroidery suit", price: 850, variations: ['Peach', 'Yellow', 'Pink', 'Sea green'] },
+    { name: "Patch lawn suit", img: "./productDetails/patchLawn.jpg", description: "Pure lawn cotton suit Fancy embroidery with patch  Cotton dupatta", price: 1000, variations: ['Blue', 'Yellow', 'Sea green', 'Black'] },
+    { name: "Party wear gowns", img: "./productDetails/Party wear gowns.jpg", description: "Net fabric with heavy embroidery Santoon lining", price: 800, variations: ['Royal Blue', 'Pista green'] },
+    { name: "Gun gun collection", img: "./productDetails/Gun gun collection.jpg", description: "", price: 500, variations: ['Tomato Red', 'Pink', 'Turmeric Yellow', 'Green'] },
+    { name: "Jamm cotton suit", img: "./productDetails/jamm cotton suit.jpg", description: "Pure jamm cotton with hand embroidery 2mt top 2.5mt botton Embroidered dupatta", price: 525, variations: ['Red', 'Orange', 'Yellow', 'Green'] },
+    { name: "Lehanga", img: "./productDetails/Lehanga.jpg", description: "net fabric lehanga with heavy embroidery design bell lace heavy dupatta,adjustable blouse with latkan and doori", price: 1000, variations: ['Green'] },
+    { name: "Mumtaz Suit", img: "./productDetails/Mumtaz Suit.png", description: "Very Famous suit", price: 650, variations: [] },
+    { name: "South cotton", img: "./productDetails/south cotton.jpg", description: "Print cotton bottom Print Dupatta", price: 750, variations: ['Purple','Orange'] }
     ]
 const reviews = [
     { author: 'Ishpreet Kaur', review: 'Discount Queen ek nayi soch ke sath kaam kar rahi hai, baki koi website community ke liye kaam nahi karti hai. yahan mujhe discount bhi milta hai aur good feel bhi donate karne ka..' },
